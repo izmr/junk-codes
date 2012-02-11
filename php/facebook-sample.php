247a20187c4bc4ee20d339f52b5e59b0e0e5aa21
marimofire <marimofire@gmail.com>
2012-02-11 16:28:10 +0900

    add php

---
+++ php/facebook-sample.php
@@ -0,0 +1,63 @@
+<?php
+require_once("../vender/php-sdk/src/facebook.php");
+
+$appId = '237330303002639';
+$secret = '90d99c801d438101e6aefcb10152f871';
+$config = array();
+$config['appId'] = $appId;
+$config['secret'] = $secret;
+
+$facebook = new Facebook($config);
+$loginParams = array('scope' => 'user_birthday');
+$statusParam = array(
+  'ok_session' => 'Now Login',
+  'no_user' => 'No User',
+  'no_session' => 'No Session'
+);
+?>
+<html>
+<head>
+<title>Facebook Test</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+<?php if ($facebook->getUser()): ?>
+<a href="<?php echo $facebook->getLogoutUrl($loginParams); ?>">ログアウト</a><br />
+<?
+$fql = 'SELECT pic,name,birthday,sex from user where uid = me()';
+$me = $facebook->api(array('method' => 'fql.query', 'query' => $fql));
+?>
+<img src="<? echo $me[0]['pic'] ?>" /><br />
+Name: <? echo $me[0]['name'] ?><br />
+Birthday: <? echo $me[0]['birthday'] ?><br />
+Sex: <? echo $me[0]['sex'] ?>
+<hr />
+<? else: ?>
+<a href="<?php echo $facebook->getLoginUrl($loginParams); ?>">ログイン</a><br />
+<? endif; ?>
+<?php if ( $facebook->getUser() ) {
+	$user_id = $facebook->getUser();
+	$fql = 'select pic_small,name,sex,birthday_date from user where uid IN (SELECT uid2 from friend where uid1 = me()) and birthday_date != ""';
+	$ret = $facebook->api(
+		array('method' => 'fql.query', 
+		'query' => $fql)); ?>
+<table>
+<tr>
+<th>Picture</th>
+<th>Name</th>
+<th>Birthday</th>
+<th>Sex</th>
+</tr>
+<? foreach ( $ret as $user ) { ?>
+<tr>
+<td><img src="<? echo $user['pic_small'] ?>" /></td>
+<td><? echo $user['name'] ?></td>
+<td><? echo $user['birthday_date'] ?></td>
+<td><? echo $user['sex'] ?></td>
+</tr>
+
+<?} ?>
+</table>
+<? } ?>
+</body>
+</html>
